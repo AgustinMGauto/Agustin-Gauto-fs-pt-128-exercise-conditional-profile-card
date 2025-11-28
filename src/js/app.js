c486481590b7d98a70da75942e8d0ca1439f9a61
AgustinMGauto <agustin_gauto@outlook.com>
2025-11-28 19:23:35 +0000

    finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -36,15 +36,23 @@ function render(variables = {}) {
           <h1>${variables.name ? variables.name : "Nombre"} ${
     variables.lastName ? variables.lastName : "Apellido"
   }</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="${
-              variables.background
-            }https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h2>${variables.role ? variables.role : "Roll"}</h2>
+          <h3>${variables.city ? variables.city : "Ciudad"} ${
+    variables.country ? variables.country : "Pais."
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4geeksacademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
